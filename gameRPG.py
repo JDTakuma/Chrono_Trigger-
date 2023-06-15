a4115cfc454e2802db6042b257a6265729f4f709
Jaime Ibarra <utm22030687@gmail.com>
2023-06-14 22:08:53 -0600

    feat attack

--- gameRPG.py
+++ gameRPG.py
@@ -1,10 +1,13 @@
 import random
 
-class Character: # Create the class "character" 
+class Character: # Create the class "character"(Father) 
     def __init__(self, name, strength, defense, healing_items):
+#Characteristics for any character 
         self.name = name
         self.strength = strength
         self.defense = defense
         self.healing_items = healing_items
-
-#Chrono_Trigger-
+ 
+ def attack(self, target): #Define funtion of attack
+        damage = self.strength - target.defense #How the "attack" works is defined 
+        target.receive_damage(damage) #result of "strength minus defense"
