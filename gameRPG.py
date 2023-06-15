6d3481d7bec56b6d2bc8975af1663e3d2d3cb383
Jaime Ibarra <utm22030687@gmail.com>
2023-06-14 21:44:57 -0600

    first commit/class

---
+++ gameRPG.py
@@ -0,0 +1,10 @@
+import random
+
+class Character:
+    def __init__(self, name, strength, defense, healing_items):
+        self.name = name
+        self.strength = strength
+        self.defense = defense
+        self.healing_items = healing_items
+
+#Chrono_Trigger-
