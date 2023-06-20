70e8a9bda6948dbfcc8716ea1cf81c0a4e147caf
Jaime Ibarra <utm22030687@gmail.com>
2023-06-20 13:50:34 -0600

    fix/structure,health,items

--- gameRPG.py
+++ gameRPG.py
@@ -1,68 +1,86 @@
 import random
 
 class Character: # Create the class "character"(Father) 
-    def __init__(self, name, strength, weaknees, heal_items):
-#Characteristics for any character 
+    def __init__(self, name, strength, weaknees, heal_items):#Characteristics for any character 
+
         self.name = name #name character
         self.strength = strength 
         self.weaknees = weaknees
-        self.heal_items = [40,40] #Only two healing items per character
-        self.life = 150 #maximum life
+        self.use_healing_items = [40, 40] #Only two healing items per character
+        self.health = 150 #maximum health
 
  #operation of the attack
     def attack(self, enemy): 
-        damage = random.randint(5, 20)
-        #Implementation of weakness and strength
-        if enemy.weakness == self.strength: 
+        damage = random.randint(5, 20) #Implementation of weakness and strength
+        if enemy.weaknees == self.strength: 
             damage *= 2 # Only if the strength is the same that weakness
             print("¡Effective attack!") # Indicates that the attack was effective
-        enemy.life -= damage 
+        self.health -= damage 
         print(f"{self.name} attacks {enemy.name} does {damage} points of damage.") #Result
 
     def defend(self): #Funtion of defense 
         defense = random.randint(10, 20) #Its more like a healing system 
-        self.life += defense 
-        if self.life > 150: 
-            self.life = 150 # The restored life cannot be greater than the maximum life of the character
-        print(f"{self.name} has defended himself and healed {defense} points of life.")
+        player2.health += defense 
+        if player2.health > 150: 
+            player2.health = 150 # The restored life cannot be greater than the maximum life of the character
+        print(f"{player1.name} has defended himself and healed {defense} points of life.")
 
     def use_healing_item(self):
-        if self.heal_items:
-            healing = self.heal_items.pop(0) #Removes the element that is in the position
-            if self.life > 150:
-                self.life = 150 # The restored life cannot be greater than the maximum life of the character
-            print(f"{self.name} #has used a healing item and has healed  {healing} points of life.")
+        if player2.use_healing_items:
+            healing = player2.use_healing_items.pop(0) #Removes the element that is in the position
+            player2.health += healing
+            if player2.health > 150:
+                player2.health = 150 # The restored life cannot be greater than the maximum life of the character
+            print(f"{player1.name} has used a healing item and has healed  {healing} points of life.")
         else:
             print("No healing objets left.") #They don´t have more healing objects
 #Characters
 class Chrono(Character): #Is the character we are going to use 
     def __init__(self): #Function to call the father class and all its properties 
-        super().__init__("Chrono", "Warrior", "Wizard")# Name, strength and weakness 
+        super().__init__("Chrono", "Warrior", "Wizard", [40,40])# Name, strength and weakness 
 
 class Marle(Character):# She is the CPU
     def __init__(self): #Function to call the father class and all its properties
-        super().__init__("Marle", "Wizard", "Warrior") # Name, strength and weakness
+        super().__init__("Marle", "Wizard", "Warrior", [40,40]) # Name, strength and weakness
+   
+    def battle(self):
+        while player1.health > 0 and player2.health > 0: #The condition for continuing to play the game (or finish the game)
+       #Basic menu      
+            print("------")
+            print(f"{player1.name} (health: {player2.health}) (class: {player1.weaknees})")
+            print(f"{player2.name} (health: {player1.health}) (class: {player2.weaknees})")
+            print("------")
+            print("1. Attack") #Action options 
+            print("2. Defend")
+            print("3. Use a healing item") 
+            choice = input("Choose a action: ") #Decide on one of the three actions 
+#Actions to choose in the basic menu 
+            if choice == "1":
+                player1.attack(player2) #Objetive to attack
+            elif choice == "2":
+                player1.defend() #Recover points of life 
+            elif choice == "3":
+                player1.use_healing_item() #use one object  
+            else:
+                print("Invalid option. Try again.") # Miss input        
+        
+            if player2.health <= 0:
+                print(f"{player2.name} wins the combat .")
+                break
 
-def main():# All features in two final objects 
-    player1 = Chrono()
-    player2 = Marle()
+#the current status of the characters is shown
+            print("------")
+            print(f"{player1.name} (health: {player2.health})") #Current health
+            print(f"{player2.name} (health: {player1.health})") #Current health
+            print("------")
+            print(f"{player2.name} is attacking...") 
+            player2.attack(player1) #Objetive
 
-    while player1.life > 0 and player2.life > 0: #The condition for continuing to play the game (or finish the game)
-       #Basic menu
-        print("------")
-        print(f"{player1.name} (Life: {player1.heal})")
-        print(f"{player2.name} (Life: {player2.heal})")
-        print("------")
-        print("1. Attack") #Action options 
-        print("2. Defend")
-        print("3. Use a healing item") 
-        choice = input("Choose a action : ") #Decide on one of the three actions 
-#Actions to choose i the basic menu 
-        if choice == "1":
-            player1.attack(player2) #Objetive to attack
-        elif choice == "2":
-            player1.defend()
-        elif choice == "3":
-            player1.use_healing_item()
-        else:
-            print("Invalid option. Try again.")
+            if player1.health <= 0: #Condition to win 
+                print(f"{player1.name} wins the combat.")
+                break 
+    
+    #Start the game
+player1 = Chrono()
+player2 = Marle()
+player2.battle()
